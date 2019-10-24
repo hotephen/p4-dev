930fd4f87a4c57c78f31432756e6f5809089544d
unknown <ghcks1000@gmail.com>
2019-10-24 19:37:47 +0900

    commit

--- util/send_nsh_10000.py
+++ util/send_nsh_10000.py
@@ -16,7 +16,7 @@ usage : send_nsh_10000.py [src] [dst] [interface] [spi] [si] [number of packets]
 the 6 arguments are needed
 """
 
-class NSH(Packet):
+class NSH(Packet, spi, si):
     """Network Service Header.
        NSH MD-type 1 if there is no ContextHeaders"""
     name = "NSH"
@@ -31,8 +31,8 @@ class NSH(Packet):
         BitField('MDType', 1, 4),
         ByteField("NextProto", 0x65),
         ByteField("NextProto_2", 0x58),
-        X3BytesField('SPI', sys.argv[4]),
-        ByteField('SI', hex(int(sys.argv[5])))
+        X3BytesField('SPI', spi),
+        ByteField('SI', hex(si))
     ]
 
 def main():
@@ -60,7 +60,7 @@ def main():
     out_ether = Ether(src=get_if_hwaddr(iface), dst='00:00:00:00:00:01', type=0x894f)
     in_ether =  Ether(src=get_if_hwaddr(iface), dst='00:00:00:00:00:01', type=0x800)
 
-    pkt1 = out_ether / NSH() / in_ether / IP(src=addr,dst=addr1) / "hi"
+    pkt1 = out_ether / NSH(spi=spi, si=si) / in_ether / IP(src=addr,dst=addr1) / "hi"
     pkt1.show()
     #hexdump(pkt1)
     
